c089d766601e8580e42195d88a0e03bcff820d23
Monica <87416567+Monica101401@users.noreply.github.com>
2026-01-18 20:47:13 +1100

    hosting version

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Portfolio', 
+  assetPrefix: '/Portfolio/',
+}
+
+module.exports = nextConfig
 
 export default nextConfig;
